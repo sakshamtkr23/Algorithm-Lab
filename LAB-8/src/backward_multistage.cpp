1e544bb5352346ebf84d38e8fa6aac013796a323
sakshamtkr23 <saksham.tkr23@gmail.com>
2026-04-29 03:41:19 +0530

    Add files via upload

---
+++ LAB-8/src/backward_multistage.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define N 8
+#define INF 9999
+
+int main()
+{
+    int cost[N][N] = {
+        {0, 1, 2, 5, 0, 0, 0, 0},
+        {0, 0, 0, 0, 4, 11, 0, 0},
+        {0, 0, 0, 0, 9, 5, 16, 0},
+        {0, 0, 0, 0, 0, 0, 2, 0},
+        {0, 0, 0, 0, 0, 0, 0, 18},
+        {0, 0, 0, 0, 0, 0, 0, 13},
+        {0, 0, 0, 0, 0, 0, 0, 2},
+        {0, 0, 0, 0, 0, 0, 0, 0}};
+
+    int dist[N], path[N];
+
+    for (int i = 0; i < N; i++)
+        dist[i] = INF;
+
+    dist[N - 1] = 0;
+
+    for (int i = N - 2; i >= 0; i--)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (cost[i][j] != 0 && dist[i] > cost[i][j] + dist[j])
+            {
+                dist[i] = cost[i][j] + dist[j];
+                path[i] = j;
+            }
+        }
+    }
+
+    cout << "Minimum Cost: " << dist[0] << endl;
+
+    cout << "Path: ";
+    int temp = 0;
+    while (temp != N - 1)
+    {
+        cout << temp << " -> ";
+        temp = path[temp];
+    }
+    cout << N - 1;
+
+    return 0;
+}
